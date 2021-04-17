32c114bdd1e3a93b360edf4551985bf00e3910ce
LucasAxelson <lucasaxelson2016@gmail.com>
2021-04-17 10:10:34 +0100

    User input registered

--- border-radius/app.js
+++ border-radius/app.js
@@ -1,5 +1,16 @@
-const borderRadius = document.querySelector(`.input__radius`)
+const borderRadius = document.querySelector(`.btn__radius`)
+
 const box = document.querySelector(`.box`)
 
 // ==================================================================
 
+borderRadius.addEventListener(`click`, function (e) {
+    e.preventDefault()
+
+    let selectRadius = document.querySelector(`.select__radius`).value
+    let inputRadius = document.querySelector(`.input__radius`).value
+    let userInput = inputRadius + selectRadius
+
+    
+    console.log(userInput)
+})
